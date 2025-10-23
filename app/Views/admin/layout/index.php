77f08863e20157d1e8c1d332f734417d0cd4c2d5
rendy <rendy@paycools.co.id>
2025-10-23 23:21:30 +0700

    update title

--- app/Views/admin/layout/index.php
+++ app/Views/admin/layout/index.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="robots" content="noindex, nofollow">
-  <title>MNC Logistic | Dashboard</title>
+  <title>MNC Logistic - Management Information System</title>
 
 
   <!-- jQuery -->
